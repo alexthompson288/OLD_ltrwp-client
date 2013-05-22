697f54497c19c1c35e8430d1a3260a092a776a3d
Dave Amphlett <dave@36peas.com>
2013-05-22 19:04:08 +0100

    dragdropanswer script

---
+++ Assets/_scripts/DragDropAnswer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using System.Collections;
+
+public class DragDropAnswer : MonoBehaviour {
+
+	public bool isCorrect=false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
