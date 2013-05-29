180916cac001ca7d771184d1ec05cd4cefd01fb0
Dave Amphlett <dave@36peas.com>
2013-05-29 21:49:13 +0100

    All of the things. Sequencer and SkillIntro updates

---
+++ Assets/_scenes/SkillIntroduction/SkillIntroductionManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class SkillIntroductionManager : MonoBehaviour {
+	
+	public Transform Rod;
+	public float StartRodTime;
+	
+	float timePassed;
+	
+	// Use this for initialization
+	void Start () {
+//		Rod.gameObject.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		if(timePassed<=StartRodTime)
+//			timePassed+=Time.deltaTime;
+//		
+//		if(timePassed>StartRodTime && !Rod.gameObject.activeSelf)
+//		{
+//			Rod.gameObject.SetActive(true);
+//			Rod.gameObject.renderer.enabled=true;
+//		}
+	}
+}
