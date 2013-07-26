5aebba908bbca936cb813faf95eddb72ae37b2f0
Dave Amphlett <dave@36peas.com>
2013-07-26 18:15:34 +0100

    add in sound button to storypage

--- Assets/_scenes/ContentBrowser/StoryBrowser/Scripts/StoryManager.cs
+++ Assets/_scenes/ContentBrowser/StoryBrowser/Scripts/StoryManager.cs
@@ -10,9 +10,13 @@ public class StoryManager : MonoBehaviour {
 	public AudioClip EndClip;
 	public Transform TheEnd;
 	bool shownTheEnd=false;
+	bool pageHasAudio;
 	public OTTextSprite txtTL;
 	public OTTextSprite txtTC;
 	public OTTextSprite txtTR;
+	public OTSprite btnPlay;
+	public Texture2D btnPlayPress;
+	public Texture2D btnPlayUnpress;
 
 	void Awake () {
 		CreateNewPersistentObject();
@@ -30,13 +34,24 @@ public class StoryManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(pageHasAudio){
+			if(audio.isPlaying && btnPlay.image==btnPlayUnpress)
+			{
+				btnPlay.image=btnPlayPress;
+			}
+			else if(!audio.isPlaying && btnPlay.image==btnPlayPress)
+			{
+				btnPlay.image=btnPlayUnpress;
+			}
+		}
 	}
 
 	void NextPage () {
 		txtTL.visible=false;
 		txtTC.visible=false;
 		txtTR.visible=false;
+		btnPlay.visible=false;
+		pageHasAudio=false;
 		int modPageIndex=CurrentPage+1;
 		StoryPageData thisPage=GameManager.Instance.GetStoryPageFor(PersistentManager.StoryID, modPageIndex);
 		Debug.Log("thisPage Image Images/storypages/"+thisPage.ImageName);
@@ -77,6 +92,8 @@ public class StoryManager : MonoBehaviour {
 
 		if(thisPage.AudioName!="")
 		{
+			btnPlay.visible=true;
+			pageHasAudio=true;
 			AudioClip ac=(AudioClip)Resources.Load("audio/stories/"+thisPage.AudioName);
 			audio.clip=ac;
 			audio.Play();
@@ -124,7 +141,13 @@ public class StoryManager : MonoBehaviour {
 
 	void On_SimpleTap(Gesture gesture)
 	{
-		NextPage();
+		if(gesture.pickObject!=null)
+		{
+			if(gesture.pickObject.name=="btnPlay" && !audio.isPlaying)
+				audio.Play();
+			else 
+				NextPage();
+		}
 	}
 
 	void On_SwipeEnd(Gesture gesture)
