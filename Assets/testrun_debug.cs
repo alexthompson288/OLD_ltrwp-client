94d8dd7e744266c596b64864eb3413b6e42f5394
Dave Amphlett <dave@36peas.com>
2013-07-29 15:31:25 +0100

    testscene sprite

--- Assets/testrun_debug.cs
+++ Assets/testrun_debug.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class testrun_debug : MonoBehaviour {
 
+	public OTSprite thisSprite;
+
 	// Use this for initialization
 	void Start () {
 		Debug.Log("running");
+		thisSprite.image=(Texture2D)Resources.Load("Images/mnemonics_images_png_250/a_angry_ant");
 	}
 	
 	// Update is called once per frame
